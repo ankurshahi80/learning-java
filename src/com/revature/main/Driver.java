e70d4e04ea0dc6fec07733e69166209a0dd667b1
Ankur Shahi <shahiankur80@gmail.com>
2022-03-18 22:20:38 -0400

    first constructor - Person object

--- src/com/revature/main/Driver.java
+++ src/com/revature/main/Driver.java
@@ -1,15 +1,30 @@
 package com.revature.main;
 
+import com.revature.model.Person;
+
 public class Driver {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
         methodA();
+        methodB("passing a String");
+
+
+        Person p1 = new Person();
+        p1.firstName = "John";
+        p1.lastName = "Doe";
+        p1.age = 40;
+
+        p1.speak();
 
         System.out.println("End of main() method.");
     }
 
     public static void methodA() {
         System.out.println("Running methodA()!");
+    }
+
+    public static void methodB(String myString){
+        System.out.println("My string: " + myString);
     }
 }
---
+++ src/com/revature/model/Person.java
@@ -0,0 +1,12 @@
+package com.revature.model;
+
+public class Person {
+
+    public String firstName;
+    public String lastName;
+    public int age;
+
+    public void speak(){
+        System.out.println("Hi, my name is " + firstName + ".");
+    }
+}
