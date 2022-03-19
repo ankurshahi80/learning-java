62f128c7394274b6bf7d02bb3967e568e1ac29c4
Ankur Shahi <shahiankur80@gmail.com>
2022-03-18 22:04:39 -0400

    Hello, World

---
+++ src/com/revature/main/Driver.java
@@ -0,0 +1,15 @@
+package com.revature.main;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        methodA();
+
+        System.out.println("End of main() method.");
+    }
+
+    public static void methodA() {
+        System.out.println("Running methodA()!");
+    }
+}
